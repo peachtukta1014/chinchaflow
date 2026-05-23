8d23bd1ca5a7ae20e83371389d381ac075f9d455
peachtukta1014 <peachtukta1014@gmail.com>
2026-05-23 21:57:42 +0700

    feat(seafood): add Bangkok date helpers

---
+++ apps/seafood-pos/src/lib/date.js
@@ -0,0 +1,7 @@
+export function dateKeyBangkok(date = new Date()) {
+  return new Date(date.getTime() + 7 * 3600000).toISOString().split('T')[0];
+}
+
+export function tomorrowDateKeyBangkok(date = new Date()) {
+  return new Date(date.getTime() + 7 * 3600000 + 86400000).toISOString().split('T')[0];
+}
