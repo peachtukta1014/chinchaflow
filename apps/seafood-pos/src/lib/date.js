ce960c7f0e5c1607ca90147a8e85ad22c949ebf0
peachtukta1014 <peachtukta1014@gmail.com>
2026-05-23 21:58:50 +0700

    fix(seafood): expose Bangkok date helpers globally

--- apps/seafood-pos/src/lib/date.js
+++ apps/seafood-pos/src/lib/date.js
@@ -5,3 +5,8 @@ export function dateKeyBangkok(date = new Date()) {
 export function tomorrowDateKeyBangkok(date = new Date()) {
   return new Date(date.getTime() + 7 * 3600000 + 86400000).toISOString().split('T')[0];
 }
+
+if (typeof window !== 'undefined') {
+  window.dateKeyBangkok = dateKeyBangkok;
+  window.tomorrowDateKeyBangkok = tomorrowDateKeyBangkok;
+}
