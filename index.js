554b5ad1c1d658399934d90521cdfe16c450c58d
peachtukta1014 <260092759+peachtukta1014@users.noreply.github.com>
2026-06-19 02:10:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { execSync } from 'child_process';
 import { fileURLToPath } from 'url';
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
